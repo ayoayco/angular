db6884ef2bc4d0372ecb68637ad3044aa37a2b09
Ayo <mail@ayoayco.com>
2016-02-02 16:04:36 +0800

    [gem store] added products, made screen responsive

--- app.js
+++ app.js
@@ -4,17 +4,23 @@
 
 
 	app.controller('StoreController', function(){
-		this.products = gems;
+		this.products = gems; 
+		this.sortString = "price";
+		this.sortItems = function(){
+			if(this.sortString === "price") this.sortString = "-price";
+			else this.sortString = "price";
+		}
 	});
 
 
 	var gems = [
 		{
 			name: 'Dodecahedron',
-			price: '2.95',
+			price: '2',
 			description: 'Best gem ever! Really!',
 			canPurchase: true,
 			soldOut: false,
+			images:'1.png'	
 		},
 		{
 			name: 'Pentagonal Gem',
@@ -22,6 +28,23 @@
 			description: 'Great Gem too.',
 			canPurchase: true,
 			soldOut: false,
+			images:'2.png'
+		},
+		{
+			name: 'Sorcerer\'s Gem',
+			price: '5.95',
+			description: 'Great Gem too.',
+			canPurchase: true,
+			soldOut: false,
+			images:'3.png'
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: '5.95',
+			description: 'Great Gem too.',
+			canPurchase: true,
+			soldOut: false,
+			images:'4.png'
 		}
 	]
 
