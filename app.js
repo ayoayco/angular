6d249b213766672b0a78dc60e85c9dbae53a56b2
Ayo <mail@ayoayco.com>
2016-02-22 17:43:17 +0800

    can't use json. huhu

--- app.js
+++ app.js
@@ -21,102 +21,99 @@
         };
     });
     
-    var gems =  [
-		        {
-		            "name": "Petty Soul Gem",
-		            "price": 2,
-		            "description": "Best gem ever! Really!",
-		            "specs": "specs specs specs specs .",
-		            "reviews": [
-				        {
-				            "stars": 5,
-				            "body": "Review Review Review.",
-				            "author": "Ayo Ayco",
-				            "createdOn": 12345643
-				        },
-				        {
-				            "stars": 5,
-				            "body": "Review Review Review.",
-				            "author": "Ayo Ayco",
-				            "createdOn": 12345643
-				        }
-		            ],
-		            "canPurchase": true,
-		            "soldOut": false,
-		            "images": "1.png"
-		        },
-		        {
-		            "name": "Common Soul Gem",
-		            "price": 5.50,
-		            "description": "Great Gem too.",
-		            "specs": "specs specs specs specs .",
-		            "reviews": [
-				        {
-				            "stars": 5,
-				            "body": "Review Review Review.",
-				            "author": "Ayo Ayco",
-				            "createdOn": 12345643
-				        },
-				        {
-				            "stars": 5,
-				            "body": "Review Review Review.",
-				            "author": "Ayo Ayco",
-				            "createdOn": 12345643
-				        }
-		            ],
-		            "canPurchase": true,
-		            "soldOut": false,
-		            "images": "2.png"
-		        },
-		        {
-		            "name": "Sorcerer\'s Gem",
-		            "price": 13.00,
-		            "description": "Great Gem too.",
-		            "specs": "specs specs specs specs .",
-		            "reviews": [
-				        {
-				            "stars": 5,
-				            "body": "Review Review Review.",
-				            "author": "Ayo Ayco",
-				            "createdOn": 12345643
-				        },
-				        {
-				            "stars": 5,
-				            "body": "Review Review Review.",
-				            "author": "Ayo Ayco",
-				            "createdOn": 12345643
-				        }
-		            ],
-		            "canPurchase": true,
-		            "soldOut": false,
-		            "images": "3.png"
-		        },
-          {
-              "name": "Fine
-            Gem",
-            "price": 7.25,
-            "description": "Great Gem too.",
-            "specs": "Specs Specs Specs .",
-            "reviews": [
-              {
-                  "stars": 5,
-                  "body": "Review
-                Review Review.",
-                "author": "Ayo Ayco",
-                "createdOn": 12345643
-        },
-              {
-                  "stars": 5,
-                  "body": "Review Review Review.",
-                  "author": "Ayo Ayco",
-                  "createdOn": 12345643
-              }
-        ],
-            "canPurchase": true,
-            "soldOut": false,
-            "images": "4.png"
+    var gems = [
+            {
+                name: 'Petty Soul Gem',
+                price: 2,
+                description: 'Best gem ever! Really!',
+                specs: 'Specs Specs Specs.',
+                reviews: [
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    },
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    }
+                ],
+                canPurchase: true,
+                soldOut: false,
+                images: '1.png'
+            },
+            {
+                name: 'Common Soul Gem',
+                price: 5.50,
+                description: 'Great Gem too.',
+                specs: 'Specs Specs Specs.',
+                reviews: [
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    },
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    }
+                ],
+                canPurchase: true,
+                soldOut: false,
+                images: '2.png'
+            },
+            {
+                name: 'Sorcerer\'s Gem',
+                price: 13.00,
+                description: 'Great Gem too.',
+                specs: 'Specs Specs Specs.',
+                reviews: [
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    },
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    }
+                ],
+                canPurchase: true,
+                soldOut: false,
+                images: '3.png'
+            },
+            {
+                name: 'Fine Gem',
+                price: 7.25,
+                description: 'Great Gem too.',
+                specs: 'Specs Specs Specs.',
+                reviews: [
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    },
+                    {
+                        stars: 5,
+                        body: 'Review Review Review.',
+                        author: 'Ayo Ayco',
+                        createdOn: 12345643
+                    }
+                ],
+                canPurchase: true,
+                soldOut: false,
+                images: '4.png'
             }
-
     ];
 
 })();
