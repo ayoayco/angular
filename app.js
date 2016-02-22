8962bff1af729e9ef81481ccc72483820c542552
Ayo <mail@ayoayco.com>
2016-02-22 17:37:50 +0800

    can't use json huhu

--- app.js
+++ app.js
@@ -4,13 +4,7 @@
 
     app.controller('StoreController',['$http', function ($http) {
         var store = this;
-        store.products = [];
-
-        $http.get('http://ayoayco.com/angular/products.json').then(function(data){
-            store.products = data;
-        }, function () {
-            alert("Loading Products JSON Failed.");
-        });
+        store.products = gems;
 
         store.sortItems = function () {
             if (this.sortString === "price") this.sortString = "-price";
@@ -27,4 +21,102 @@
         };
     });
     
+    var gems =  [
+		        {
+		            "name": "Petty Soul Gem",
+		            "price": 2,
+		            "description": "Best gem ever! Really!",
+		            "specs": "specs specs specs specs .",
+		            "reviews": [
+				        {
+				            "stars": 5,
+				            "body": "Review Review Review.",
+				            "author": "Ayo Ayco",
+				            "createdOn": 12345643
+				        },
+				        {
+				            "stars": 5,
+				            "body": "Review Review Review.",
+				            "author": "Ayo Ayco",
+				            "createdOn": 12345643
+				        }
+		            ],
+		            "canPurchase": true,
+		            "soldOut": false,
+		            "images": "1.png"
+		        },
+		        {
+		            "name": "Common Soul Gem",
+		            "price": 5.50,
+		            "description": "Great Gem too.",
+		            "specs": "specs specs specs specs .",
+		            "reviews": [
+				        {
+				            "stars": 5,
+				            "body": "Review Review Review.",
+				            "author": "Ayo Ayco",
+				            "createdOn": 12345643
+				        },
+				        {
+				            "stars": 5,
+				            "body": "Review Review Review.",
+				            "author": "Ayo Ayco",
+				            "createdOn": 12345643
+				        }
+		            ],
+		            "canPurchase": true,
+		            "soldOut": false,
+		            "images": "2.png"
+		        },
+		        {
+		            "name": "Sorcerer\'s Gem",
+		            "price": 13.00,
+		            "description": "Great Gem too.",
+		            "specs": "specs specs specs specs .",
+		            "reviews": [
+				        {
+				            "stars": 5,
+				            "body": "Review Review Review.",
+				            "author": "Ayo Ayco",
+				            "createdOn": 12345643
+				        },
+				        {
+				            "stars": 5,
+				            "body": "Review Review Review.",
+				            "author": "Ayo Ayco",
+				            "createdOn": 12345643
+				        }
+		            ],
+		            "canPurchase": true,
+		            "soldOut": false,
+		            "images": "3.png"
+		        },
+          {
+              "name": "Fine
+            Gem",
+            "price": 7.25,
+            "description": "Great Gem too.",
+            "specs": "Specs Specs Specs .",
+            "reviews": [
+              {
+                  "stars": 5,
+                  "body": "Review
+                Review Review.",
+                "author": "Ayo Ayco",
+                "createdOn": 12345643
+        },
+              {
+                  "stars": 5,
+                  "body": "Review Review Review.",
+                  "author": "Ayo Ayco",
+                  "createdOn": 12345643
+              }
+        ],
+            "canPurchase": true,
+            "soldOut": false,
+            "images": "4.png"
+            }
+
+    ];
+
 })();
